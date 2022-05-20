982b95cd5f26416a46d66ed8092b8d26c2afea42
AnjaliDeshwani <deshwanianjali@gmail.com>
2022-05-20 20:08:42 +0530

    fixed active color issue in categories

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -14,7 +14,7 @@ export const Filter = ({ categoryName }) => {
   return (
     <div
       className={`py-1 px-3 rounded-lg bg-slate-900 dark:bg-slate-50 dark:text-slate-900 text-slate-50 cursor-pointer hover:bg-sky-400 self-baseline ${
-        isActive(categoryName) ? "bg-sky-400" : ""
+        isActive(categoryName) ? "bg-sky-400 dark:bg-sky-400" : ""
       }`}
       onClick={() =>
         videoDispatch({
